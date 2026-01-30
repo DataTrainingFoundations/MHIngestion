f16ef63ddd37d88d0e834f4ef93803842a51ef4d
ritvikkona8 <ritvikkona8@gmail.com>
2026-01-30 14:05:11 -0500

    Update AppConfig.py

--- AppConfig.py
+++ AppConfig.py
@@ -6,15 +6,15 @@ class AppConfig:
     _instance = None
     _data = None
 
-    def __init__(self):
-        with open(APP_CONFIG_FILENAME,'r') as file:
-            self._data = yaml.safe_load(file)
-
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(AppConfig, cls).__new__(cls)
+            with open(APP_CONFIG_FILENAME, 'r') as file:
+                cls._instance._data = yaml.safe_load(file)
+        return cls._instance
+        
     def __getitem__(self, key):
         return self._data[key]
-    
 
 if __name__ == "__main__":
-    appConfig : AppConfig = AppConfig()
-    print(appConfig['database']['connection']['host'])
-
+    print(AppConfig()['database']['connection']['host'])
