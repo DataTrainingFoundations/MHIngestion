648c9ba58833a7a38a8b67b90028bef493eb29f8
Mishimoo <mishra.ryan.p@gmail.com>
2026-02-05 14:24:07 -0500

    added Validator and changed Reader
    Reader now outputs a dataframe and validator removes rows with suppression marked, phase -1, and any missing required fields

--- ingestion/Reader.py
+++ ingestion/Reader.py
@@ -1,25 +1,22 @@
 import pandas as pd
+import logging
 
-class DataReader():
+def read_data(data_path):
+    try:
+        # data_path: str = "data/Raw Data/Mental_Health_DB.csv"
+        # takes a csv and turns it into a pandas dataframe
+        df = pd.read_csv(data_path)
 
-    def __init__(self):
-        self.data_path: str = "data/Raw Data/Mental_Health_DB.csv"
-    
-    def read_data(self) -> list[dict]:
-        try:
-            # takes a csv and turns it into a pandas dataframe
-            df = pd.read_csv(self.data_path)
-            # turns that dataframe into a list of dicts
-            records = df.to_dict(orient="records")
+        # logs successful reading
+        # ("read " + len(records) + f" rows from source file {self.data_path}") 
 
-            # logs successful reading
-            # ("read " + len(records) + f" rows from source file {self.data_path}") 
-
-            return records
+        return df
             
             
-        except:
-            print("something went wrong")
+    except:
+        print("something went wrong")
+
+
 
 
 
